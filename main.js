fe8b9d86e6bdc3737070c589c57cf2ea796badcd
KandoSVK <kubo.kanitra@gmail.com>
2016-05-02 21:16:36 +0200

    Fix bad callback params

--- main.js
+++ main.js
@@ -3,7 +3,11 @@
 var downloadComicBookImages = require('./app/downloader').downloadComicBookImages;
 var createBook = require('./app/pdf').createBook;
 
-downloadComicBookImages('deadpool', '1', function(imgPaths){
+downloadComicBookImages('deadpool', '1', function(err, imgPaths){
+  if (err) {
+    console.log(err);
+    return;
+  }
   imgPaths = imgPaths.sort();
   createBook('./issues/deadpool', 'Deadpool#1', imgPaths);
   console.log(`Deadpool #1 DOWNLOADED!!!`);
